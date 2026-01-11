79cebe6c18cfbc4164a6c396c1cd1a147ee1d423
shivangp0208 <patelshivang702@gmail.com>
2026-01-11 14:44:57 +0000

    initialized the envoy manager service with spring data web,jpa and mysql-connector

---
+++ src/main/java/com/project/hems/envoy_manager_service/EnvoyManagerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.project.hems.envoy_manager_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnvoyManagerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnvoyManagerServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/hems/envoy_manager_service/EnvoyManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.hems.envoy_manager_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnvoyManagerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
